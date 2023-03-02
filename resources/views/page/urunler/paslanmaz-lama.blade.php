a27dd2a203ea92a84e4d0bb30aa0339160c55374
sitesepetim <84999348+sitesepetim@users.noreply.github.com>
2023-03-02 17:32:35 +0300

    lama

--- resources/views/page/urunler/paslanmaz-lama.blade.php
+++ resources/views/page/urunler/paslanmaz-lama.blade.php
@@ -38,6 +38,307 @@
                         İlk olarak istenilen kaliteye uygun olan formüllere göre paslanmaz çelik hurdaları eritilir.  Bu eritme sırasında
                         kayıplar olması durumda formüle göre amalgam elementleri ilave edilerek paslanmaz kütükleri dökülür. Dökülen kütükler
                         sıcak haddelemeye alınır ve sıcak haldeki kütükler makaralar arasından geçirilerek istenilen  boyda kesilir ve sonuç elde edilir </div>
+
+                    <br>
+                    <br>
+                    <h5><strong>Üretimdeki Paslanmaz Lama ve Teorik Birim Ağırlıkları(Kg/Mt)</strong></h5>
+                    <br>
+                    <table class="table table-striped">
+                        <thead>
+                        <tr >
+                            <th scope="col">MM</th>
+                            <th scope="col">3</th>
+                            <th scope="col">4</th>
+                            <th scope="col">5</th>
+                            <th scope="col">6</th>
+                            <th scope="col">8</th>
+                            <th scope="col">10</th>
+                            <th scope="col">12</th>
+                            <th scope="col">15</th>
+                            <th scope="col">20</th>
+                            <th scope="col">25</th>
+                            <th scope="col">30</th>
+                            <th scope="col">40</th>
+                            <th scope="col">50</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">10</th>
+                            <td>0,236</td>
+                            <td>0,314</td>
+                            <td>0,393</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">15</th>
+                            <td>0,353</td>
+                            <td>0,471</td>
+                            <td>0,589</td>
+                            <td>0,707</td>
+                            <td>0,942</td>
+                            <td>1,178</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">20</th>
+                            <td>0,471</td>
+                            <td>0,628</td>
+                            <td>0,785</td>
+                            <td>0,942</td>
+                            <td>1,256</td>
+                            <td>1,571</td>
+                            <td>1,844</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">25</th>
+                            <td>0,589</td>
+                            <td>0,785</td>
+                            <td>0,981</td>
+                            <td>1,178</td>
+                            <td>1,571</td>
+                            <td>1,965</td>
+                            <td>2,355</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">30</th>
+                            <td>0,707</td>
+                            <td>0,942</td>
+                            <td>1,178</td>
+                            <td>1,413</td>
+                            <td>1,884</td>
+                            <td>2,355</td>
+                            <td>2,826</td>
+                            <td>3,533</td>
+                            <td>4,711</td>
+                            <td>5,888</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">35</th>
+                            <td>0,824</td>
+                            <td>1,099</td>
+                            <td>1,374</td>
+                            <td>1,649</td>
+                            <td>2,198</td>
+                            <td>2,748</td>
+                            <td>3,297</td>
+                            <td>4,121</td>
+                            <td>5,495</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">40</th>
+                            <td>0,942</td>
+                            <td>1,256</td>
+                            <td>1,571</td>
+                            <td>1,884</td>
+                            <td>2,512</td>
+                            <td>3,141</td>
+                            <td>3,758</td>
+                            <td>4,711</td>
+                            <td>6,280</td>
+                            <td>7,851</td>
+                            <td>9,421</td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">45</th>
+                            <td>1,061</td>
+                            <td>1,413</td>
+                            <td>1,%1006</td>
+                            <td>2,120</td>
+                            <td>2,826</td>
+                            <td>3,533</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">50</th>
+                            <td>1,178</td>
+                            <td>1,571</td>
+                            <td>1,963</td>
+                            <td>2,355</td>
+                            <td>3,141</td>
+                            <td>3,925</td>
+                            <td>4,711</td>
+                            <td>5,888</td>
+                            <td>7,851</td>
+                            <td>9,813</td>
+                            <td>11,770</td>
+                            <td>15,701</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">60</th>
+                            <td>1,413</td>
+                            <td>1,884</td>
+                            <td>2,355</td>
+                            <td>2,826</td>
+                            <td>3,%1008</td>
+                            <td>4,711</td>
+                            <td>5,652</td>
+                            <td>7,065</td>
+                            <td>9,421</td>
+                            <td>9,421</td>
+                            <td>14,130</td>
+                            <td>18,841</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">70</th>
+                            <td>1,649</td>
+                            <td>2,198</td>
+                            <td>2,748</td>
+                            <td>3,297</td>
+                            <td>4,396</td>
+                            <td>5,495</td>
+                            <td>6,594</td>
+                            <td>8,243</td>
+                            <td>10,991</td>
+                            <td>13,757</td>
+                            <td>16,490</td>
+                            <td>21,981</td>
+                            <td>27,481</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">80</th>
+                            <td>1,884</td>
+                            <td>2,512</td>
+                            <td>3,141</td>
+                            <td>3,%1008</td>
+                            <td>5,024</td>
+                            <td>6,281</td>
+                            <td>7,536</td>
+                            <td>9,421</td>
+                            <td>12,561</td>
+                            <td>15,701</td>
+                            <td>18,840</td>
+                            <td>25,121</td>
+                            <td>31,401</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">90</th>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>5,652</td>
+                            <td>7,065</td>
+                            <td>8,478</td>
+                            <td>10,601</td>
+                            <td>14,131</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">100</th>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>3,925</td>
+                            <td>4,711</td>
+                            <td>6,281</td>
+                            <td>7,851</td>
+                            <td>9,421</td>
+                            <td>11,771</td>
+                            <td>15,701</td>
+                            <td>19,621</td>
+                            <td>23,550</td>
+                            <td>31,401</td>
+                            <td>39,251</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">120</th>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>4,710</td>
+                            <td>5,652</td>
+                            <td>7,536</td>
+                            <td>9,421</td>
+                            <td>11,301</td>
+                            <td>14,131</td>
+                            <td>18,841</td>
+                            <td>23,551</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">150</th>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>5,887</td>
+                            <td>7,065</td>
+                            <td>9,421</td>
+                            <td>11,771</td>
+                            <td>14,131</td>
+                            <td>17,661</td>
+                            <td>23,551</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">200</th>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>12,561</td>
+                            <td>15,701</td>
+                            <td>18,841</td>
+                            <td>23,550</td>
+                            <td>31,401</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+
+                        <br>
+                        <br>
+
                     <h3>PASLANMAZ LAMA KULLANIM ALANLARI</h3>
                     <div class="text">Gelişen teknolojiler sanayi ve teknolojiyle beraber paslanmaz lama malzemelerin kullanımı  giderek artmaktadır.
                         Hijyenik özellikte olmaları nedeniyle gıda endüstrisinde tercih edilir. Bu özelliği sayesinde gıda sektörü için vazgeçilmezdir.
