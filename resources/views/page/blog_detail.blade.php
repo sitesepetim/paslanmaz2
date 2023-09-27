50591ff0ffb702d34be5d7e558bf458794707d27
sitesepetim <sitesepetim@gmail.com>
2023-09-27 09:33:47 +0300

    Update blog_detail.blade.php

--- resources/views/page/blog_detail.blade.php
+++ resources/views/page/blog_detail.blade.php
@@ -6,23 +6,23 @@
     <meta charset="utf-8">
     <title>Desenlerimiz | Huzey Metal</title>
     <!-- Stylesheets -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
-    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="{{asset('assets/css/bootstrap.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
     <!-- Responsive File -->
-    <link href="assets/css/responsive.css" rel="stylesheet">
+    <link href="{{asset('assets/css/responsive.css')}}" rel="stylesheet">
     <!-- Color File -->
-    <link href="assets/css/color.css" rel="stylesheet">
+    <link href="{{asset('assets/css/color.css')}}" rel="stylesheet">
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&amp;display=swap" rel="stylesheet">
 
-    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
-    <link rel="icon" href="assets/images/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="{{asset('assets/images/favicon.png')}}" type="image/x-icon">
+    <link rel="icon" href="{{asset('assets/images/favicon.png')}}" type="image/x-icon">
 
     <!-- Responsive -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
-    <!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+    <!--[if lt IE 9]><script src="{{asset('js/respond.js')}}"></script><![endif]-->
 </head>
 
 <body>
@@ -102,24 +102,23 @@
 <!--Scroll to top-->
 <div class="scroll-to-top scroll-to-target" data-target="html"><span class="flaticon-right-arrow-4"></span></div>
 
-<script src="assets/js/jquery.js"></script>
-<script src="assets/js/popper.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/bootstrap-select.min.js"></script>
-<script src="assets/js/jquery.fancybox.js"></script>
-<script src="assets/js/isotope.js"></script>
-<script src="assets/js/owl.js"></script>
-<script src="assets/js/appear.js"></script>
-<script src="assets/js/wow.js"></script>
-<script src="assets/js/lazyload.js"></script>
-<script src="assets/js/scrollbar.js"></script>
-<script src="assets/js/TweenMax.min.js"></script>
-<script src="assets/js/swiper.min.js"></script>
-<script src="assets/js/jquery.polyglot.language.switcher.js"></script>
-<script src="assets/js/jquery.ajaxchimp.min.js"></script>
-<script src="assets/js/parallax-scroll.js"></script>
-
-<script src="assets/js/script.js"></script>
+<script src="{{asset('assets/js/jquery.js')}}"></script>
+<script src="{{asset('assets/js/popper.min.js')}}"></script>
+<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/js/bootstrap-select.min.js')}}"></script>
+<script src="{{asset('assets/js/jquery.fancybox.js')}}"></script>
+<script src="{{asset('assets/js/isotope.js')}}"></script>
+<script src="{{asset('assets/js/owl.js')}}"></script>
+<script src="{{asset('assets/js/appear.js')}}"></script>
+<script src="{{asset('assets/js/wow.js')}}"></script>
+<script src="{{asset('assets/js/lazyload.js')}}"></script>
+<script src="{{asset('assets/js/scrollbar.js')}}"></script>
+<script src="{{asset('assets/js/TweenMax.min.js')}}"></script>
+<script src="{{asset('assets/js/swiper.min.js')}}"></script>
+<script src="{{asset('assets/js/jquery.polyglot.language.switcher.js')}}"></script>
+<script src="{{asset('assets/js/jquery.ajaxchimp.min.js')}}"></script>
+<script src="{{asset('assets/js/parallax-scroll.js')}}"></script>
+<script src="{{asset('assets/js/script.js')}}"></script>
 
 
 </body>
